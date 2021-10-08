1cb6cfdb4a6c96675c29bce86039d5cd624d077c
GitHub Action <action@github.com>
2021-10-08 15:41:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/noahrichards/millisplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/noahrichards/millisplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.noahrichards.millisplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.noahrichards.millisplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/noahrichards/millisplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/noahrichards/millisplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.noahrichards.millisplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.noahrichards.millisplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
